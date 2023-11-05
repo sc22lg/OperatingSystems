9732132ee91cd0d2a258d2c8adad5b6dbb939809
Leo Gott [sc22lg] <sc22lg@feng-linux-08.leeds.ac.uk>
2023-11-05 21:00:22 +0000

    sucsessfully separating the command into words

--- user/my_shell.c
+++ user/my_shell.c
@@ -2,6 +2,8 @@
 #include "kernel/param.h"
 #include "user/user.h"
 
+#define MAX_WORD_LENGTH 50
+
 int main(void) {
     //make a whole shell :)
     
@@ -22,11 +24,53 @@ int main(void) {
         printf(">>>");
         read(0, b, sizeof(in_buff));
 
+        //write each word into argv
+        char argv[MAXARG][50];
+        int wordCount = 0;
+        char buffer[MAX_WORD_LENGTH];
+        int bufferIndex = 0;
+        int inWord = 0;  // Flag to track if we are inside a word
+
         while(*b != '\0'){
-            printf("%c", *b);
+            //printf("%c", *b);
+
+            char currentChar = *b;
+
+            if (currentChar != ' ') {
+                buffer[bufferIndex] = currentChar;
+                bufferIndex++;
+                inWord = 1;
+            } else if (inWord) {
+                buffer[bufferIndex] = '\0';  // Null-terminate the word
+                for (int j = 0; buffer[j] != '\0'; j++) {
+                    argv[wordCount][j] = buffer[j];
+                }
+                argv[wordCount][bufferIndex] = '\0';  // Null-terminate the word in the result array
+                wordCount++;
+                bufferIndex = 0;
+                inWord = 0;
+            }
             b++;
+
+        }
+
+        if (inWord) {
+            // Handle the last word if it's not followed by a space
+            buffer[bufferIndex] = '\0';  // Null-terminate the last word
+            for (int j = 0; buffer[j] != '\0'; j++) {
+                argv[wordCount][j] = buffer[j];
+            }
+            argv[wordCount][bufferIndex] = '\0';  // Null-terminate the last word in the result array
+            wordCount++;
         }
         
+        // Print the separated words
+        for (int i = 0; i < wordCount; i++) {
+            printf("Word %d: %s\n", i + 1, argv[i]);
+        }
+
+        int argc = wordCount;
+        printf("%d\n", argc);
 
     }
     return 0;
