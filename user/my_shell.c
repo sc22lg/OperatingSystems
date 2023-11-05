959c6c5efcb83c9a14d6bb38a04396fcab5f8d0e
Leo Gott [sc22lg] <sc22lg@feng-linux-08.leeds.ac.uk>
2023-11-05 19:41:10 +0000

    Taking user input sucssessfully

--- user/my_shell.c
+++ user/my_shell.c
@@ -4,5 +4,30 @@
 
 int main(void) {
     //make a whole shell :)
+    
+    int shell_running = 1;
+
+    //setup our input buffer
+    char in_buff[512], *b;
+    for(int i = 0; i < sizeof(in_buff); i++){
+        in_buff[i] = '\0';
+    }
+
+    b = in_buff;
+
+    //a loop which asks for a command then executes the user input
+    while(shell_running == 1){
+
+        //take an input from the user
+        printf(">>>");
+        read(0, b, sizeof(in_buff));
+
+        while(*b != '\0'){
+            printf("%c", *b);
+            b++;
+        }
+        
+
+    }
     return 0;
 }
